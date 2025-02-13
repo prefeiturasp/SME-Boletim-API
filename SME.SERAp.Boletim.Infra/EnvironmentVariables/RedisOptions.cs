69394934535eef46015ac564605f7d2e6bcee35d
Wesley Silva <wesley_gyn@yahoo.com.br>
2025-02-13 15:31:20 -0300

    AB#125892
    Alterar sessão da variável de ambiente

--- SME.SERAp.Boletim.Infra/EnvironmentVariables/RedisOptions.cs
+++ SME.SERAp.Boletim.Infra/EnvironmentVariables/RedisOptions.cs
@@ -9,7 +9,7 @@ namespace SME.SERAp.Boletim.Infra.EnvironmentVariables
 {
     public class RedisOptions
     {
-        public static string Secao => "RedisOptions";
+        public static string Secao => "Redis";
         public string Endpoint { get; set; }
         public Proxy Proxy { get; set; }
         public int SyncTimeout { get; set; } = 5000;
