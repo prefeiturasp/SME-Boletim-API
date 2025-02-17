c9c3c2a51d4de881b21c37d8990a6dfb7585eade
wesleygyn <wesley_gyn@yahoo.com.br>
2025-02-17 11:10:38 -0300

    Ajuste program.cs remove IsDevelopment

--- SME.SERAp.Boletim.Api/Program.cs
+++ SME.SERAp.Boletim.Api/Program.cs
@@ -73,11 +73,14 @@ RegistraDependencias.Registrar(builder.Services);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
