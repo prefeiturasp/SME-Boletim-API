899aef051164696fe9b87ed3379223eb3d544a70
Caique dos Santos Latorre Siqueira <caique.siqueira@spassu.com.br>
2025-03-11 18:06:13 -0300

    AB#126365

--- SME.SERAp.Boletim.Aplicacao/Queries/ObterBoletimEscolarPorUe/ObterBoletimEscolarPorUeQuery.cs
+++ SME.SERAp.Boletim.Aplicacao/Queries/ObterBoletimEscolarPorUe/ObterBoletimEscolarPorUeQuery.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using SME.SERAp.Boletim.Dominio.Entidades;
+using SME.SERAp.Boletim.Infra.Dtos.Boletim;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,13 @@ namespace SME.SERAp.Boletim.Aplicacao.Queries.ObterBoletimEscolarPorUe
 {
     public class ObterBoletimEscolarPorUeQuery : IRequest<IEnumerable<BoletimEscolar>>
     {
-        public ObterBoletimEscolarPorUeQuery(long ueId)
+        public ObterBoletimEscolarPorUeQuery(long ueId, FiltroBoletimDto filtros)
         {
             UeId = ueId;
+            Filtros = filtros;
         }
         public long UeId { get; set; }
+
+        public FiltroBoletimDto Filtros { get; set; }
     }
 }
