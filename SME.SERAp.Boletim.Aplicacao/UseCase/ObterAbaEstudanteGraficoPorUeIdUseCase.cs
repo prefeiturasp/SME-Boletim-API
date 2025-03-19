58ca79e7e48fd35ee72c2761a9bd0e3963b5844b
wesleygyn <wesley_gyn@yahoo.com.br>
2025-03-19 10:38:18 -0300

    AB#126238 Validação UE

--- SME.SERAp.Boletim.Aplicacao/UseCase/ObterAbaEstudanteGraficoPorUeIdUseCase.cs
+++ SME.SERAp.Boletim.Aplicacao/UseCase/ObterAbaEstudanteGraficoPorUeIdUseCase.cs
@@ -1,7 +1,10 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Components.Forms;
 using SME.SERAp.Boletim.Aplicacao.Interfaces.UseCase;
 using SME.SERAp.Boletim.Aplicacao.Queries.ObterAbaEstudanteBoletimEscolarGraficoPorUeId;
+using SME.SERAp.Boletim.Aplicacao.Queries.ObterUesAbrangenciaUsuarioLogado;
 using SME.SERAp.Boletim.Infra.Dtos.BoletimEscolar;
+using SME.SERAp.Boletim.Infra.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +24,12 @@ namespace SME.SERAp.Boletim.Aplicacao.UseCase
 
         public async Task<IEnumerable<AbaEstudanteGraficoDto>> Executar(long ueId)
         {
+            var abrangenciasUsuarioLogado = await _mediator
+                .Send(new ObterUesAbrangenciaUsuarioLogadoQuery());
+
+            if (!abrangenciasUsuarioLogado?.Any(x => x.UeId == ueId) ?? true)
+                throw new NaoAutorizadoException("Usuário não possui abrangências para essa UE.");
+
             return await _mediator.Send(new ObterAbaEstudanteGraficoPorUeIdQuery(ueId));
         }
     }
