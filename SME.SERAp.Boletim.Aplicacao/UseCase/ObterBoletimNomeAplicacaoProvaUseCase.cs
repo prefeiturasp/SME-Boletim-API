044799d1d944795786d706a93a4a7f0188a235af
Mario Junior <mariosj9@gmail.com>
2025-03-21 16:32:39 -0300

    AB#126528

---
+++ SME.SERAp.Boletim.Aplicacao/UseCase/ObterBoletimNomeAplicacaoProvaUseCase.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SME.SERAp.Boletim.Aplicacao.Interfaces.UseCase;
+using SME.SERAp.Boletim.Aplicacao.Queries.ObterLoteProvaAtivo;
+using SME.SERAp.Boletim.Infra.Dtos.LoteProva;
+
+namespace SME.SERAp.Boletim.Aplicacao.UseCase
+{
+    public class ObterBoletimNomeAplicacaoProvaUseCase : IObterBoletimNomeAplicacaoProvaUseCase
+    {
+        private readonly IMediator mediator;
+
+        public ObterBoletimNomeAplicacaoProvaUseCase(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public  Task<LoteProvaAtivoDto> Executar()
+        {
+            return mediator.Send(new ObterLoteProvaAtivoQuery());
+        }
+    }
+}
