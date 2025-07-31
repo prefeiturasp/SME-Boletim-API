8209fbe39a741ab923783b0ce77a1c4cc9e3dddb
Mario Junior <mariosj9@gmail.com>
2025-07-31 10:11:10 -0300

    AB#130946

---
+++ SME.SERAp.Boletim.Aplicacao/Queries/ObterDownloadProvasBoletimEscolarSme/ObterDownloadProvasBoletimEscolarSmeQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using SME.SERAp.Boletim.Dados.Interfaces;
+using SME.SERAp.Boletim.Infra.Dtos.BoletimEscolar;
+
+namespace SME.SERAp.Boletim.Aplicacao.Queries
+{
+    public class ObterDownloadProvasBoletimEscolarSmeQueryHandler : IRequestHandler<ObterDownloadProvasBoletimEscolarSmeQuery, IEnumerable<DownloadProvasBoletimEscolarPorDreDto>>
+    {
+        private readonly IRepositorioBoletimEscolar repositorio;
+        public ObterDownloadProvasBoletimEscolarSmeQueryHandler(IRepositorioBoletimEscolar repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        public async Task<IEnumerable<DownloadProvasBoletimEscolarPorDreDto>> Handle(ObterDownloadProvasBoletimEscolarSmeQuery request, CancellationToken cancellationToken)
+            => await repositorio.ObterDownloadProvasBoletimEscolarSme(request.LoteId);
+    }
+}
