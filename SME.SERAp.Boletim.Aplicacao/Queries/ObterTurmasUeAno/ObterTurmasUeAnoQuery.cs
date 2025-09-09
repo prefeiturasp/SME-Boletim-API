ba6fd30b149aa4c5dcfa64ab6f2dca8f8a8d801d
Mario Junior <mariosj9@gmail.com>
2025-09-09 14:32:25 -0300

    AB#133598

---
+++ SME.SERAp.Boletim.Aplicacao/Queries/ObterTurmasUeAno/ObterTurmasUeAnoQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SME.SERAp.Boletim.Infra.Dtos.BoletimEscolar;
+
+namespace SME.SERAp.Boletim.Aplicacao.Queries
+{
+    public class ObterTurmasUeAnoQuery : IRequest<IEnumerable<TurmaAnoDto>>
+    {
+        public long LoteId { get; set; }
+        public long UeId { get; set; }
+        public int DisciplinaId { get; set; }
+        public int AnoEscolar { get; set; }
+
+        public ObterTurmasUeAnoQuery(long loteId, long ueId, int disciplinaId, int anoEscolar)
+        {
+            LoteId = loteId;
+            UeId = ueId;
+            DisciplinaId = disciplinaId;
+            AnoEscolar = anoEscolar;
+        }
+    }
+}
