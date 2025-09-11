7545d6a28f57aad4b82420dbcfed3ba4276e1906
Mario Junior <mariosj9@gmail.com>
2025-09-11 11:44:57 -0300

    AB#133599

---
+++ SME.SERAp.Boletim.Aplicacao/UseCase/ObterTurmasUeAnoUseCase.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using SME.SERAp.Boletim.Aplicacao.Interfaces.UseCase;
+using SME.SERAp.Boletim.Aplicacao.Queries;
+using SME.SERAp.Boletim.Aplicacao.Queries.ObterUesAbrangenciaUsuarioLogado;
+using SME.SERAp.Boletim.Infra.Dtos.BoletimEscolar;
+using SME.SERAp.Boletim.Infra.Exceptions;
+
+namespace SME.SERAp.Boletim.Aplicacao.UseCase
+{
+    public class ObterTurmasUeAnoUseCase : IObterTurmasUeAnoUseCase
+    {
+        private readonly IMediator mediator;
+        public ObterTurmasUeAnoUseCase(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public async Task<IEnumerable<TurmaAnoDto>> Executar(long loteId, long ueId, int disciplinaId, int anoEscolar)
+        {
+            var abrangenciasUsuarioLogado = await mediator
+                .Send(new ObterUesAbrangenciaUsuarioLogadoQuery());
+
+            if (!abrangenciasUsuarioLogado?.Any(x => x.UeId == ueId) ?? true)
+                throw new NaoAutorizadoException("Usuário não possui abrangências para essa UE.");
+
+            return await mediator.Send(new ObterTurmasUeAnoQuery(loteId, ueId, disciplinaId, anoEscolar));
+        }
+    }
+}
